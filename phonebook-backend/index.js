afc5a962c5c01fbc17ea806a2adf125f435eeabd
preitinger <peter.reitinger@gmail.com>
2022-12-12 23:17:47 +0100

    3.6, 3.7, 3.8 complete

--- phonebook-backend/index.js
+++ phonebook-backend/index.js
@@ -1,7 +1,17 @@
 const generateId = require('./generateId')
 const express = require('express')
+const morgan = require('morgan')
+
 const app = express()
 app.use(express.json())
+
+morgan.token(
+  "person",
+  (req, res) => {
+    return JSON.stringify(req.body)
+  }
+)
+app.use(morgan(":method :url :status :res[content-length] - :response-time ms :person"));
 const port = 3001
 
 app.get('/', (req, res) => {
@@ -59,8 +69,31 @@ app.delete("/api/persons/:id", (req, res) => {
   res.status(204).end();
 })
 
+const err400 = (res, errorText) => {
+  return res.status(400).json({error: errorText});
+}
+
 app.post("/api/persons", (req, res) => {
   const person = req.body;
+
+  if (!person) {
+    return err400(res, "No data provided");
+  }
+
+  if (!person.name) {
+    return err400(res, "name missing");
+  }
+
+  if (!person.number) {
+    return err400(res, "number missing");
+  }
+
+  const oldPerson = persons.find(p => p.name === person.name);
+
+  if (oldPerson) {
+    return err400(res, "name must be unique");
+  }
+
   person.id = generateId.next();
   persons.push(person); // why not here, because no requirement for persons being immutable
   console.log("post: person=", person);
