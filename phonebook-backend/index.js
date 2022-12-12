25913ffcf5b81b2d0cc4c9d3d2cb751f2b0a5d87
preitinger <peter.reitinger@gmail.com>
2022-12-12 22:05:48 +0100

    3.4 complete

--- phonebook-backend/index.js
+++ phonebook-backend/index.js
@@ -7,7 +7,7 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-const persons = [
+let persons = [
     {
       "id": 1,
       "name": "Arto Hellas",
@@ -40,7 +40,6 @@ app.get('/info', (req, res) => {
 
 app.get("/api/persons/:id", (req, res) => {
   const id = Number(req.params.id);
-  console.log("id", id);
   const person = persons.find(p => p.id === id);
 
   if (person) {
@@ -48,6 +47,14 @@ app.get("/api/persons/:id", (req, res) => {
   } else {
     res.status(404).end();
   }
+})
+
+app.delete("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const person = persons.find(p => p.id === id);
+  console.log("deleting ", person);
+  persons = persons.filter(p => p.id !== id);
+  res.status(204).end();
 })
 
 app.listen(port, () => {
