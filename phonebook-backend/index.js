5608e2932c7f35b9fdf91d4a1d7bb42cb4fe588c
preitinger <peter.reitinger@gmail.com>
2022-12-12 21:53:27 +0100

    3.2 complete

--- phonebook-backend/index.js
+++ phonebook-backend/index.js
@@ -7,32 +7,37 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+const persons = [
+    {
+      "id": 1,
+      "name": "Arto Hellas",
+      "number": "040-123456"
+    },
+    {
+      "id": 2,
+      "name": "Ada Lovelace",
+      "number": "39-44-5323523"
+    },
+    {
+      "id": 3,
+      "name": "Dan Abramov",
+      "number": "12-43-234345"
+    },
+    {
+      "id": 4,
+      "name": "Mary Poppendieck",
+      "number": "39-23-6423122"
+    }
+]
+
 app.get("/api/persons", (req, res) => {
-  res.json(
-    [
-        {
-          "id": 1,
-          "name": "Arto Hellas",
-          "number": "040-123456"
-        },
-        {
-          "id": 2,
-          "name": "Ada Lovelace",
-          "number": "39-44-5323523"
-        },
-        {
-          "id": 3,
-          "name": "Dan Abramov",
-          "number": "12-43-234345"
-        },
-        {
-          "id": 4,
-          "name": "Mary Poppendieck",
-          "number": "39-23-6423122"
-        }
-    ]
-  )
+  res.json(persons);
 })
+
+app.get('/info', (req, res) => {
+  res.send(`<p>Phonebook has info for ${persons.length} people</p><p>${new Date().toString()}</p>`)
+})
+
 app.listen(port, () => {
   console.log(`phonebook app listening on port ${port}`)
 })
